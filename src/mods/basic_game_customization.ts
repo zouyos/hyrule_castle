a16ca80c9dc17496c82ab11c5d568f777a442776
benket_h <hamza.benketaf@gmail.com>
2024-10-22 18:25:44 +0000

    v3: MOD added: Basic Game Customization

---
+++ src/mods/basic_game_customization.ts
@@ -0,0 +1,192 @@
+import * as playersFromJson from '../../data/players.json';
+import * as enemiesFromJson from '../../data/enemies.json';
+import * as bossesFromJson from '../../data/bosses.json';
+
+const rl = require('readline-sync');
+
+type Char = {
+  name: string,
+  hp: number,
+  hpMax: number,
+  mp: number,
+  str: number,
+  int: number,
+  def: number,
+  res: number,
+  spd: number,
+  luck: number,
+  race: number,
+  class: number,
+  rarity: number,
+  isPlayer: boolean,
+  isBoss: boolean,
+};
+
+const players: Char[] = [...playersFromJson].map((player) => ({
+  ...player,
+  isPlayer: true,
+  isBoss: false,
+  hpMax: player.hp,
+}));
+
+const enemies: Char[] = [...enemiesFromJson].map((enemy) => ({
+  ...enemy,
+  isPlayer: false,
+  isBoss: false,
+  hpMax: enemy.hp,
+}));
+
+const bosses: Char[] = [...bossesFromJson].map((boss) => ({
+  ...boss,
+  isPlayer: false,
+  isBoss: true,
+  hpMax: boss.hp,
+}));
+
+function displayHp(char: Char) {
+  const hpArray: string[] = Array(char.hpMax).fill('I');
+  hpArray.forEach((hp, i) => {
+    if (char.hpMax !== char.hp && i + 1 > char.hp) {
+      hpArray[i] = '.';
+    }
+  });
+  console.log(`${char.isPlayer ? '\u001b[32m' : '\u001b[31m'}${char.name}'s HP: [${hpArray.join('')}] ${char.hp >= 0 ? char.hp : '0'}/${char.hpMax}\u001b[37m\n`);
+}
+
+function fight(player: Char, enemy: Char) {
+  console.log(`You encounter ${enemy.isBoss ? enemy.name : `a \u001b[31m${enemy.name}\u001b[37m`}, prepare to fight!\n`);
+  while (!(enemy.hp <= 0 || player.hp <= 0)) {
+    displayHp(player);
+    displayHp(enemy);
+    const move = rl.question('==== Options:  1.Attack  2.Heal  ====\n\n');
+    if (move === '1') {
+      enemy.hp -= player.str;
+      console.log(`\nYou attack \u001b[31m${enemy.name}\u001b[37m!\n`);
+      console.log(`${enemy.name} loses ${player.str} HP.\n`);
+    } else if (move === '2') {
+      if (player.hp + Math.round(player.hpMax / 2) <= player.hpMax) {
+        console.log(`\n\u001b[34mYou recover ${Math.round(player.hpMax / 2)} HP.\u001b[37m\n`);
+        player.hp += Math.round(player.hpMax / 2);
+      } else {
+        1
+        console.log(`\n\u001b[34mYou recover ${player.hpMax - player.hp} HP.\u001b[37m\n`);
+        player.hp = player.hpMax;
+      }
+    }
+    if (enemy.hp !== 0) {
+      console.log(`${enemy.name} attacks!\n`);
+      player.hp -= enemy.str;
+      console.log(`You lose ${enemy.str} HP.\n`);
+    }
+  }
+  if (enemy.hp <= 0) {
+    displayHp(enemy);
+    console.log(`${enemy.name} DEFEATED!\n`);
+  }
+  if (player.hp <= 0) {
+    displayHp(player);
+    console.log('GAME OVER\n');
+    process.exit();
+  }
+}
+
+function game(player: Char, monsters: Char[], boss: Char) {
+  let updatedMonsters: Char[] = []
+  let updatedBoss: Char = boss
+  console.log('\n\u001b[37m==== Welcome to Hyrule Castle Game ====\n\n');
+  const newGame = rl.question('[1] New Game\n[2] Quit\n\n');
+  if (newGame === '1') {
+    console.log('\nPlease select a difficulty:\n');
+    const difficulty = rl.question('[1] Normal\n[2] Difficult\n[3] Insane\n\n');
+    if (difficulty === '2') {
+      updatedMonsters = monsters.map((monster) => {
+        return Object.fromEntries(
+          Object.entries(monster).map(([key, val]) => [
+            key,
+            typeof val === 'number' ? Math.round(val * 1.5) : val,
+          ])
+        ) as Char;
+      });
+      updatedBoss = Object.fromEntries(
+        Object.entries(boss).map(([key, val]) => [
+          key,
+          typeof val === 'number' ? Math.round(val * 1.5) : val,
+        ])
+      ) as Char;
+    } else if (difficulty === '3') {
+      updatedMonsters = monsters.map((monster) => {
+        return Object.fromEntries(
+          Object.entries(monster).map(([key, val]) => [
+            key,
+            typeof val === 'number' ? Math.round(val * 2) : val,
+          ])
+        ) as Char;
+      });
+      updatedBoss = Object.fromEntries(
+        Object.entries(boss).map(([key, val]) => [
+          key,
+          typeof val === 'number' ? Math.round(val * 2) : val,
+        ])
+      ) as Char;
+    } else if (difficulty !== '1') {
+      console.log('Please provide a valid entree');
+    }
+    console.log('==== You enter Hyrule Castle ====\n');
+    for (let i = 0; i < updatedMonsters.length; i += 1) {
+      console.log(`\u001b[33m==== FLOOR ${i + 1} ====\u001b[37m\n`);
+      fight(player, updatedMonsters[i]);
+    }
+    console.log('\u001b[35m==== BOSS FLOOR ====\u001b[37m\n');
+    fight(player, updatedBoss);
+    console.log('Congratulations, you saved Hyrule from Evil.\n');
+  } else if (newGame === '2') {
+    process.exit()
+  } else {
+    console.log('Please type a valid entree.');
+  }
+}
+
+function pickChar(chars: Char[]) {
+  const rarities = [
+    { idx: 1, pct: 50 },
+    { idx: 2, pct: 30 },
+    { idx: 3, pct: 15 },
+    { idx: 4, pct: 4 },
+    { idx: 5, pct: 1 },
+  ];
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+  let result: number | undefined;
+  let acc = 0;
+
+  rarities.forEach((rarity) => {
+    if (result === undefined && randomNumber > 100 - rarity.pct - acc) result = rarity.idx;
+    acc += rarity.pct;
+  });
+
+  const pickableChars: Char[] = [];
+
+  for (const char of chars) {
+    if (char.rarity === result) {
+      pickableChars.push({ ...char });
+    }
+  }
+
+  return pickableChars[Math.floor(Math.random() * pickableChars.length)];
+}
+
+function pickChars(arr: Char[]) {
+  const pickedChars: Char[] = [];
+  for (let i = 0; i < 9; i += 1) {
+    pickedChars.push(pickChar(arr));
+  }
+  return pickedChars;
+}
+
+const player = pickChar(players);
+
+const monsters = pickChars(enemies);
+
+const boss = pickChar(bosses);
+
+game(player, monsters, boss);
